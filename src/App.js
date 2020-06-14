415dd21c8097399e0454c9b7cb70b43c0c51aff0
charliegabriella <charlotroucha@protonmail.com>
2020-06-14 20:44:34 +0200

    Create SearchBox in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
-import { CardList } from "./components/CardList/CardList";
-import { SearchBox } from "./components/SearchBox/SearchBox.jsx";
+import {CardList} from "./components/CardList/CardList";
+import {SearchBox} from "./components/SearchBox/SearchBox.jsx";
 
 class App extends Component {
   constructor() {
@@ -15,22 +15,21 @@ class App extends Component {
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
+      .then((users) => this.setState({monsters: users}));
   }
 
   render() {
-    const { monsters, searchField } = this.state;
+    const {monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
 
     return (
-      <div className="App">
-        <input
-          type="search"
-          placeholder="search monster"
-          onChange={(e) => {
-            this.setState({ searchField: e.target.value }, () =>
+      <div className='App'>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={(e) => {
+            this.setState({searchField: e.target.value}, () =>
               console.log(this.state)
             );
           }}
