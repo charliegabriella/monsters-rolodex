a19b890df6cb13e48ff988d7e739ee69a3280690
Charlot <57155814+charliegabriella@users.noreply.github.com>
2020-06-14 19:23:37 +0000

    Styling (#3)
    * Create Index and App Style
    
    * Add google font

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
 
     return (
       <div className='App'>
+        <h1>Charlie's Monsters</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={this.handleChange}
