58f8b37c63937cdfa61ae068c54102ec3b5bc07f
charliegabriella <charlotroucha@protonmail.com>
2020-06-14 20:40:41 +0200

    Create SearchBox Function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/CardList/CardList";
+import { SearchBox } from "./components/SearchBox/SearchBox.jsx";
+
 class App extends Component {
   constructor() {
     super();
